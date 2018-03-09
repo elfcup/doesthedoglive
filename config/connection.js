28db8743b51748973a9728509b3edee9a448ee55
danicoles <colescodes@gmail.com>
2018-03-09 11:44:04 -0800

    to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,23 @@ var mysql = require('mysql');
 
 // var connection;
 
+// var connection = mysql.createConnection({
+// 	// connectionLimit: 10,	
+// 	port: 3306,
+// 	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+// 	user: 'oobcc714e2gxy2xm',
+// 	password: 'uh5g8z3z765o4qju',
+// 	database: 'dogmovies_db'
+// 	});
+
 var connection = mysql.createConnection({
 	// connectionLimit: 10,	
 	port: 3306,
-	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: 'da0kbir6uftng6tn',
-	password: 'ja5iifg131tqc0fx',
+	host: 'localhost',
+	user: 'root',
+	password: 'NeptunePluto101',
 	database: 'dogmovies_db'
-	});
+});
 
 connection.connect(function(err) {
   if (err) {
